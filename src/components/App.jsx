66b46c2e294eadbbeafcb034408bad973aed6531
Olexandr Tkachuk <temishena@gmail.com>
2023-04-03 00:54:12 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import Home from 'pages/Home/Home';
-import Login from 'pages/Login/Login';
-import Profile from 'pages/Profile/Profile';
-import Register from 'pages/Register/Register';
+// import Login from 'pages/Login/Login';
+// import Profile from 'pages/Profile/Profile';
+// import Register from 'pages/Register/Register';
 
 const App = () => {
   return (
